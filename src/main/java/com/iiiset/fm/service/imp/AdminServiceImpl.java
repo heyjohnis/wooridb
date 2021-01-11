a7db93ec52526c70dd11016bcdcc32209f058a2b
nochangjun <ncj0291@naver.com>
2021-01-12 00:53:26 +0900

    통테이전 최종수정

--- src/main/java/com/iiiset/fm/service/imp/AdminServiceImpl.java
+++ src/main/java/com/iiiset/fm/service/imp/AdminServiceImpl.java
@@ -183,64 +183,119 @@ public class AdminServiceImpl implements AdminService {
 			cell.setCellStyle(headerCellStyle);
 
 			cell = row.createCell(1);
-			cell.setCellValue("접수일자");
-			cell.setCellStyle(headerCellStyle);
-
-			cell = row.createCell(2);
-			cell.setCellValue("출처");
-			cell.setCellStyle(headerCellStyle);
-			
-			cell = row.createCell(3);
 			cell.setCellValue("고객명");
 			cell.setCellStyle(headerCellStyle);
-
-			cell = row.createCell(4);
+			
+			cell = row.createCell(2);
 			cell.setCellValue("연락처");
 			cell.setCellStyle(headerCellStyle);
 			
+			cell = row.createCell(3);
+			cell.setCellValue("문의내용");
+			cell.setCellStyle(headerCellStyle);
+
+			cell = row.createCell(4);
+			cell.setCellValue("성별");
+			cell.setCellStyle(headerCellStyle);
+			
 			cell = row.createCell(5);
-			cell.setCellValue("담당자");
+			cell.setCellValue("나이");
 			cell.setCellStyle(headerCellStyle);
 			
 			cell = row.createCell(6);
-			cell.setCellValue("주문수량");
+			cell.setCellValue("상담내용");
 			cell.setCellStyle(headerCellStyle);
 			
 			cell = row.createCell(7);
-			cell.setCellValue("금액");
+			cell.setCellValue("주소");
 			cell.setCellStyle(headerCellStyle);
 			
 			cell = row.createCell(8);
-			cell.setCellValue("제품명");
+			cell.setCellValue("구매수량");
 			cell.setCellStyle(headerCellStyle);
 			
 			cell = row.createCell(9);
-			cell.setCellValue("구매상태");
+			cell.setCellValue("구매금액");
 			cell.setCellStyle(headerCellStyle);
 			
 			cell = row.createCell(10);
-			cell.setCellValue("콜성공률");
+			cell.setCellValue("주문일자");
 			cell.setCellStyle(headerCellStyle);
 			
 			cell = row.createCell(11);
-			cell.setCellValue("콜성공수");
+			cell.setCellValue("계좌번호");
+			cell.setCellStyle(headerCellStyle);
+			
+			cell = row.createCell(12);
+			cell.setCellValue("카드번호");
+			cell.setCellStyle(headerCellStyle);
+			
+			cell = row.createCell(13);
+			cell.setCellValue("만료일자");
+			cell.setCellStyle(headerCellStyle);
+			
+			cell = row.createCell(14);
+			cell.setCellValue("담당자");
+			cell.setCellStyle(headerCellStyle);
+			
+			cell = row.createCell(15);
+			cell.setCellValue("구매접촉일");
+			cell.setCellStyle(headerCellStyle);
+			
+			cell = row.createCell(16);
+			cell.setCellValue("출처");
+			cell.setCellStyle(headerCellStyle);
+			
+			cell = row.createCell(17);
+			cell.setCellValue("등록일자");
+			cell.setCellStyle(headerCellStyle);
+			
+			cell = row.createCell(18);
+			cell.setCellValue("수정일자");
+			cell.setCellStyle(headerCellStyle);
+			
+			cell = row.createCell(19);
+			cell.setCellValue("상품명");
+			cell.setCellStyle(headerCellStyle);
+			
+			cell = row.createCell(20);
+			cell.setCellValue("구매상태");
+			cell.setCellStyle(headerCellStyle);
+			
+			cell = row.createCell(21);
+			cell.setCellValue("콜성공률");
+			cell.setCellStyle(headerCellStyle);
+			
+			cell = row.createCell(22);
+			cell.setCellValue("콜성공건수");
 			cell.setCellStyle(headerCellStyle);
 			
 			// Creating data rows for each customer
 			for (int i = 0; i < list.size(); i++) {
 				Row dataRow = sheet.createRow(i + 1);
 				dataRow.createCell(0).setCellValue(list.get(i).getSeq());
-				dataRow.createCell(1).setCellValue(list.get(i).getReg_date());
-				dataRow.createCell(2).setCellValue(list.get(i).getSite());
-				dataRow.createCell(3).setCellValue(list.get(i).getCust_nm());
-				dataRow.createCell(4).setCellValue(list.get(i).getCust_tel());
-				dataRow.createCell(5).setCellValue(list.get(i).getManager());
-				dataRow.createCell(6).setCellValue(list.get(i).getOrder_cnt());
-				dataRow.createCell(7).setCellValue(list.get(i).getOrder_amount());
-				dataRow.createCell(8).setCellValue(list.get(i).getGd_cd());
-				dataRow.createCell(9).setCellValue(list.get(i).getGd_buyType());
-				dataRow.createCell(10).setCellValue(list.get(i).getCall_succRate());
-				dataRow.createCell(11).setCellValue(list.get(i).getCall_cnt());
+				dataRow.createCell(1).setCellValue(list.get(i).getCust_nm());
+				dataRow.createCell(2).setCellValue(list.get(i).getCust_tel());
+				dataRow.createCell(3).setCellValue(list.get(i).getComment());
+				dataRow.createCell(4).setCellValue(list.get(i).getGender());
+				dataRow.createCell(5).setCellValue(list.get(i).getAge());
+				dataRow.createCell(6).setCellValue(list.get(i).getMemo());
+				dataRow.createCell(7).setCellValue(list.get(i).getAddr());
+				dataRow.createCell(8).setCellValue(list.get(i).getOrder_cnt());
+				dataRow.createCell(9).setCellValue(list.get(i).getOrder_amount());
+				dataRow.createCell(10).setCellValue(list.get(i).getOrder_date());
+				dataRow.createCell(11).setCellValue(list.get(i).getBank_account());
+				dataRow.createCell(12).setCellValue(list.get(i).getCard_number());
+				dataRow.createCell(13).setCellValue(list.get(i).getExpire_date());
+				dataRow.createCell(14).setCellValue(list.get(i).getManager());
+				dataRow.createCell(15).setCellValue(list.get(i).getFollowing_date());
+				dataRow.createCell(16).setCellValue(list.get(i).getSite());
+				dataRow.createCell(17).setCellValue(list.get(i).getReg_date());
+				dataRow.createCell(18).setCellValue(list.get(i).getMod_date());
+				dataRow.createCell(19).setCellValue(list.get(i).getGd_cd());
+				dataRow.createCell(20).setCellValue(list.get(i).getGd_buyType());
+				dataRow.createCell(21).setCellValue(list.get(i).getCall_succRate());
+				dataRow.createCell(22).setCellValue(list.get(i).getCall_cnt());
 			}
 
 			// Making size of column auto resize to fit with data
